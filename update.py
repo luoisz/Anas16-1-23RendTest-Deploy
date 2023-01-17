18cc9944e6ec675c8207f4b6b8d50846e057490a
luoisz <86519689+luoisz@users.noreply.github.com>
2023-01-17 20:47:49 +0630

    Update update.py

--- update.py
+++ update.py
@@ -13,21 +13,20 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL', '')
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
 try:
     if len(CONFIG_FILE_URL) == 0:
         raise TypeError
     try:
-        res = requests.get(CONFIG_FILE_URL)
+        res = rget(CONFIG_FILE_URL)
         if res.status_code == 200:
             with open('config.env', 'wb+') as f:
                 f.write(res.content)
-                f.close()
         else:
-            logging.error(f"Failed to download config.env {res.status_code}")
+            log_error(f"Failed to download config.env {res.status_code}")
     except Exception as e:
-        logging.error(f"CONFIG_FILE_URL: {e}")
-except TypeError:
+        log_error(f"CONFIG_FILE_URL: {e}")
+except:
     pass
 
 load_dotenv('config.env', override=True)
